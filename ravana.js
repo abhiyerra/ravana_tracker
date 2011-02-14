6482d5ad7fb388a3345841017f9c0136daec6ed2
Abhi Yerra <abhi@berkeley.edu>
2011-02-13 16:50:10 -0800

    Update the db with the changes that the peer sends.

--- ravana.js
+++ ravana.js
@@ -5,13 +5,15 @@
 
 var http = require('http');
 var url = require('url');
+var fs  = require('fs');
 
 var bencode = require('dht-bencode');
-var redis = require("redis");
-var redis_cli = redis.createClient();
+var redis = require("redis"),
+redis_cli = redis.createClient();
 
+var config = JSON.parse(fs.readFileSync("config.json"));
 
-function isAllowedPeer(ip) {
+function isAllowedPeer(peer_key) {
     // TODO: Yes this doesn't work yet!
     return redis_cli.sismember("blacklist", ip, function(err, reply) {
         if(reply == 1) {
@@ -22,75 +24,94 @@ function isAllowedPeer(ip) {
     });
 }
 
+// Do we allow this client to connect.
 function allowedTorrentClient(peer_id) {
-    // Do we allow this client to connect.
+    if(config.allowed_peers.length == 0)
+        return true;
 
-    return true;
+    return false;
+}
+
+function updateDb(info_hash, peer_key, field, value) {
+    var hash_key = info_hash + ":" + peer_key;
+    redis_cli.hset(hash_key, field, value, redis.print);
 }
 
 function handleAnnounce(req, res) {
-    trackPeer(req);
+    //trackPeer(req);
+
+    console.log(req);
 
     var parsed_url = url.parse(req['url'], true);
 
+
     try {
-        var key = parsed_url.query.key;
-        if(!key) {
-            throw "Need key."
-        }
+        // We want a key so we can keep track of the peer on our
+        // server.
+        var peer_key = parsed_url.query.key;
+        if(!peer_key || !isAllowedPeer(key))
+            throw "Need peer key or the peer isn't allowed."
 
-        // Get the info_hash or die trying.
+        // We need this so we can give the user the peers that contain
+        // it.
         var info_hash = parsed_url.query.info_hash;
+        if(!info_hash)
+            throw "Need info_hash"
 
+        // The client the the peer is using. We want to track this so
+        // we can prevent gaming.
         var peer_id = parsed_url.query.peer_id
         if(!allowedTorrentClient(peer_id)) {
             throw "peer_id bad."
         }
+        updateDb(info_hash, key, "peer_id", peer_id);
 
-        var numwant = 30;
+        // The number of peers to send. The default should be 30.
+        var numwant = config.numwant;
         if(parsed_url.query.numwant) {
             numwant = parsed_url.query.numwant;
         }
 
+        // What is the state of the peer.
         var event;
         if(parsed_url.query.event) {
             // event: started, stopped, completed
             event = parsed_url.query.event;
-
-            // TODO: Update our server with the event.
         }
+        updateDb(info_hash, key, "event", event);
 
-        var ip;
+        // We want to get the peer's ip from the request. Otherwise
+        // get it from the params.
+        var ip = req.GET_THE_IP;
         if(parsed_url.query.ip) {
-            // ip: started, stopped, completed
             ip = parsed_url.query.ip;
-        } else {
-            // TODO: Get the client ip.
-            ip = req.GET_THE_IP;
         }
-
+        updateDb(info_hash, key, "ip", ip);
 
         var port;
         if(parsed_url.query.port) {
-            // port: started, stopped, completed
             port = parsed_url.query.port;
         }
+        updateDb(info_hash, key, "port", port);
 
         var uploaded;
         if(parsed_url.query.uploaded) {
 
             uploaded = parsed_url.query.uploaded;
         }
+        updateDb(info_hash, key, "uploaded", uploaded);
 
         var downloaded;
         if(parsed_url.query.downloaded) {
             downloaded = parsed_url.query.downloaded;
         }
+        updateDb(info_hash, key, "downloaded", downloaded);
 
         var left;
         if(parsed_url.query.left) {
             left = parsed_url.query.left;
         }
+        updateDb(info_hash, key, "left", left);
 
         var trackerid;
         if(parsed_url.query.trackerid) {
@@ -110,22 +131,22 @@ function handleAnnounce(req, res) {
             response_dict.incomplete = 0;
             response_dict.peers = [];
 
-        /*
-         Shit to send:
+            /*
+              Shit to send:
 
-         failure reason (if defined only send this.)
+              failure reason (if defined only send this.)
 
-         or
+              or
 
-         interval
-         tracker id
-         complete
-         incomplete
-         peers: (dict)
-           peer id (peer_id)
-           ip
-           port
-        */
+              interval
+              tracker id
+              complete
+              incomplete
+              peers: (dict)
+              peer id (peer_id)
+              ip
+              port
+            */
             keys.forEach(function(key) {
                 redis_cli.hmget(key, "left", "ip", "port", function(left, ip, port) {
                     if(left == 0) {
