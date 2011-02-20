ac35a1d52eac69aa845efb9395c075031774a8cd
Abhi Yerra <abhi@berkeley.edu>
2011-02-19 19:07:53 -0800

    A bit better code.

--- ravana.js
+++ ravana.js
@@ -21,7 +21,12 @@ function allowedTorrentClient(peer_id) {
     return false;
 }
 
-function updateDbForPeer(peer) {
+/*
+ * Save the peer request to the database.
+ */
+function updatePeer(peer) {
+    var data = peer['info_hash'] + ':' + peer['peer_key'];
+
     for(var key in peer) {
         console.log(key);
     }
@@ -32,75 +37,82 @@ function updateDbForPeer(peer) {
     // redis_cli.hset(hash_key, field, value, redis.print);
 }
 
-function handleAnnounce(req, res) {
-    //trackPeer(req);
-
-//    console.log(req.socket.remoteAddress);
-
+/*
+ * Parse the params sent to look for the appropriate key/value
+ * pairs.
+ */
+function getPeerInfo(req) {
     var parsed_url = url.parse(req['url'], true);
 
     console.log(parsed_url);
 
+    var peer = {}
+
+    peer['peer_key'] = parsed_url.query.key;
+    if(!peer['peer_key'])
+        throw "Need peer key or the peer isn't allowed.";
+
+    peer['info_hash'] = parsed_url.query.info_hash;
+    if(!peer['info_hash'])
+        throw "Need info_hash";
+
+    peer['peer_id'] = parsed_url.query.peer_id;
+    if(!allowedTorrentClient(peer['peer_id']))
+        throw "peer_id bad.";
+
+    peer['numwant'] = config.numwant;
+    if(parsed_url.query.numwant)
+        peer['numwant'] = parsed_url.query.numwant;
+
+    if(parsed_url.query.event) // event: started, stopped, completed
+        peer['event'] = parsed_url.query.event;
+
+    peer['ip'] = req.socket.remoteAddress;
+    if(parsed_url.query.ip)
+        peer['ip'] = parsed_url.query.ip;
+
+    if(parsed_url.query.port)
+        peer['port'] = parsed_url.query.port;
+
+    if(parsed_url.query.uploaded)
+        peer['uploaded'] = parsed_url.query.uploaded;
+
+    if(parsed_url.query.downloaded)
+        peer['downloaded'] = parsed_url.query.downloaded;
+
+    if(parsed_url.query.left)
+        peer['left'] = parsed_url.query.left;
+
+    if(parsed_url.query.trackerid)
+        peer['trackerid'] = parsed_url.query.trackerid;
+
+    peer['no_peer_id'] = !!parsed_url.query.no_peer_id;
+
+    console.log(peer);
+
+    return peer;
+}
+
+function handleAnnounce(req, res) {
     try {
-        peer = {}
+        var peer = getPeerInfo(req);
 
-        peer['peer_key'] = parsed_url.query.key;
-        if(!peer['peer_key'])
-            throw "Need peer key or the peer isn't allowed.";
-
-        peer['info_hash'] = parsed_url.query.info_hash;
-        if(!peer['info_hash'])
-            throw "Need info_hash";
-
-        peer['peer_id'] = parsed_url.query.peer_id;
-        if(!allowedTorrentClient(peer['peer_id']))
-            throw "peer_id bad.";
-
-        peer['numwant'] = config.numwant;
-        if(parsed_url.query.numwant)
-            peer['numwant'] = parsed_url.query.numwant;
-
-        if(parsed_url.query.event) // event: started, stopped, completed
-            peer['event'] = parsed_url.query.event;
-
-        peer['ip'] = req.socket.remoteAddress;
-        if(parsed_url.query.ip)
-            peer['ip'] = parsed_url.query.ip;
-
-        if(parsed_url.query.port)
-            peer['port'] = parsed_url.query.port;
-
-        if(parsed_url.query.uploaded)
-            peer['uploaded'] = parsed_url.query.uploaded;
-
-        if(parsed_url.query.downloaded)
-            peer['downloaded'] = parsed_url.query.downloaded;
-
-        if(parsed_url.query.left)
-            peer['left'] = parsed_url.query.left;
-
-        if(parsed_url.query.trackerid)
-            peer['trackerid'] = parsed_url.query.trackerid;
-
-        peer['no_peer_id'] = !parsed_url.query.no_peer_id;
-
-        console.log(peer);
-
-        updateDbForPeer(peer);
+        updatePeer(peer);
 
         var response_dict = {
             'interval': 10,
-            'tracker id': peer['trackerid'],
+            'complete': 0,
+            'incomplete': 0,
+            'peers': [],
+//          'tracker id': peer['trackerid'] || '',
         };
 
+        find_keys = peer['info_hash'] + ":*";
+
+        console.log("S" + find_keys);
+
         // Find all the peers that have the same info_hash and send it to the user.
-        redis_cli.keys(peer['info_hash'] + ":*", function(err, keys) {
-            response_dict.complete   = 0;
-            response_dict.incomplete = 0;
-            response_dict.peers      = [];
-
-            console.log("size" + keys.length);
-
+        redis_cli.keys(find_keys, function(err, keys) {
             // For each of the peers to the response_dict.
             keys.forEach(function(key) {
                 redis_cli.hmget(key, "left", "ip", "port", function(left, _ip, port) {
@@ -123,19 +135,16 @@ function handleAnnounce(req, res) {
 
             response = bencode.bencode(response_dict).toString()
             console.log(response);
-
-            res.end(response);
+            res.write(response);
         });
-
-
-
     } catch(err) {
         console.log("Shit hit the fan. " + err);
 
         res.write(bencode.bencode({
             'failure reason': 'Shit hit the fan.'
         }).toString());
-        res.end("\n");
+    } finally {
+        res.end();
     }
 }
 
